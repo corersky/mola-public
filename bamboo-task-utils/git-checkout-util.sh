efe221aa9317aaae963c2560d7c243c85feace57
Steve Boardwell <steve.boardwell@datameer.com>
2016-06-30 10:49:39 +0200

    BUILD-89: added log messages.

--- bamboo-task-utils/git-checkout-util.sh
+++ bamboo-task-utils/git-checkout-util.sh
@@ -4,23 +4,27 @@ set -euo pipefail
 IFS=$'\n\t'
 
 logInfo() {
-	echo "LOG-INFO: $*"
+	echo "LOG-INFO: $@"
 }
 
 die() {
-	(>&2 echo -e "$@")
+	(>&2 echo -e "ERROR: $@")
 	exit 1
 }
 if [ -n "$bamboo_build_working_directory" ]; then
 	# pre-checkout cleaning...
-	logInfo "Cleaning bamboo_build_working_directory '$bamboo_build_working_directory'..."
-	rm -rf "$bamboo_build_working_directory/*"
-	logInfo "Listing bamboo_build_working_directory..."
+	logInfo "Listing bamboo_build_working_directory BEFORE..."
+	ls -al "$bamboo_build_working_directory"
+	logInfo "Cleaning bamboo_build_working_directory '$bamboo_build_working_directory/*'..."
+	rm -rf "$bamboo_build_working_directory"/*
+	logInfo "Listing bamboo_build_working_directory AFTER..."
 	ls -al "$bamboo_build_working_directory"
 	# checkout ad branch handling...
 	cd "$bamboo_build_working_directory"
 	logInfo "Checking out '$bamboo_planRepository_branchName' with rev '$bamboo_planRepository_revision'"
-	git clone $bamboo_planRepository_repositoryUrl .
+	git clone "$bamboo_planRepository_repositoryUrl" .
 	git checkout $bamboo_planRepository_revision
 	cd -
+else
+    die "No bamboo_build_working_directory set"
 fi
