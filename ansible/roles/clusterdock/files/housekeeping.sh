31c85bf1ecd7041d3f68701f7708eca4dabb3394
michal maxian <michal@maxian.sk>
2017-05-05 15:36:16 +0200

    housekeeping.sh changed to clean.sh

--- ansible/roles/clusterdock/files/housekeeping.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker ps | grep -v CONTAINER | awk '{print $1}' | xargs docker kill
-docker rm -v $(docker ps -a -q -f status=exited)
-docker images | grep -v REPOSITORY | awk '{print $3}' | xargs docker rmi -f
-
