67fc9023c34e995a463a4461a0cc6e3625a5fb42
michal maxian <michal@maxian.sk>
2017-05-09 13:56:14 +0200

    added repo-source for repository setup in HDP

--- ansible/roles/docker-hdp/files/clean.sh
+++ ansible/roles/docker-hdp/files/clean.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+source ./repo-source.sh
 docker-compose -f /opt/docker-hdp/examples/compose/multi-container.yml kill
---
+++ ansible/roles/docker-hdp/files/repo-source.sh
@@ -0,0 +1,4 @@
+export AMBARI_DDL_URL=https://raw.githubusercontent.com/apache/ambari/branch-2.5/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
+export AMBARI_REPO_URL=http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.5.0.3/ambari.repo
+export HDP_REPO_URL=http://public-repo-1.hortonworks.com/HDP/centos6/2.x/updates/2.6.0.3/hdp.repo
+
