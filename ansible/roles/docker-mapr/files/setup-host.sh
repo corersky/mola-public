3cc8dafde7a3c12fc324129eb22bf17707a1a8b8
michal maxian <michal@maxian.sk>
2017-05-29 12:56:37 +0200

    launch cluster modified

--- ansible/roles/docker-mapr/files/setup-host.sh
+++ ansible/roles/docker-mapr/files/setup-host.sh
@@ -4,3 +4,9 @@ ip link add bridge0 type bridge
 ip addr add 10.10.100.0/24 dev bridge0
 ip link set bridge0 up
 
+# create list of disks
+# create disks 
+# create loop devices for each disk
+
+# test if # of disks are the same as # of nodes
+
