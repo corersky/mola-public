df46d55de7068e16e4d05c92aad9eb9d9f75a7c4
michal maxian <michal@maxian.sk>
2017-05-30 14:24:05 +0200

    added stop.sh script for mapr cluster removal

---
+++ ansible/roles/docker-mapr/files/stop.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+CLUSTERNAME=$1
+
+DISKLISTFILE=diskloop.$CLUSTERNAME
+DISKLVFILE=disklv.$CLUSTERNAME # file to store used LV volumes
+
+DISKFILES=`cat $DISKLISTFILE`
+DISKNUMBER=`cat $DISKLISTFILE | wc -l`
+DISKLV=`cat $DISKLVFILE`
+
+if [ ! -f $DISKLISTFILE ]; then
+ echo "$DISKLISTFILE doesn't exist!"
+ exit
+fi
+
+if [ ! -f $DISKLVFILE ]; then
+ echo "$DISKLVFILE doesn't exist!"
+ exit
+fi
+
+if [[ -z $(which docker)  ]] ; then
+        echo " docker could not be found on this server. Please install Docker version 1.6.0 or later."
+	echo " If it is already installed Please update the PATH env variable." 
+        exit
+fi
+
+dv=$(docker --version | awk '{ print $3}' | sed 's/,//')
+if [[ $dv < 1.6.0 ]] ; then
+        echo " Docker version installed on this server : $dv.  Please install Docker version 1.6.0 or later."
+        exit
+fi
+
+# Usage Check.
+if [[ $# -ne 1 ]]
+then
+	echo " Usage : $0 ClusterName NumberOfNodes MemSize-in-kB"
+	exit
+fi
+
+DOCKER_CONTAINERS=`docker ps | grep maprtech | awk '{print $1}'`
+
+for container in ${DOCKER_CONTAINERS}
+do
+  echo "Stopping container $container:"
+  docker ps | grep $container
+  docker stop $container && echo "done." || echo "problem with stopping container."
+done
+
+for lodevice in $DISKLV
+do 
+  ### deletion with lvremove /dev/$VG/lv$CLUSTERNAME$i
+  echo "Removing loopback device $lodevice"
+  losetup -vd $lodevice && echo "$lodevice removed." || echo "ehm problem occured!"
+done
+
+for lv in  
+  echo "Removing logical volume $lv"
+  lvremove -vf $lv && echo "$lv removed." || echo "some problem with lv removal".
+done
+
+
+# LOOPDEVICES=`losetup -a`
+### dynamic load for removal (from losetup -a)
+#losetup -a | \
+#while read line; do
+#    echo $line | awk -F: '{print $1}'
+#    echo $line | sed -r '/[a-z][A-Z]*$/p'
+#done
