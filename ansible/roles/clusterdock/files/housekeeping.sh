9ba483a7ee8eaaec0356b097c1a4a243bf1c7a3c
michal maxian <michal@maxian.sk>
2017-06-08 17:34:50 +0200

    houseekeping script for CDH

--- ansible/roles/clusterdock/files/housekeeping.sh
+++ ansible/roles/clusterdock/files/housekeeping.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-docker ps | grep -v CONTAINER | awk '{print $1}' | xargs docker kill
-docker rm -v $(docker ps -a -q -f status=exited)
-docker images | grep -v REPOSITORY | awk '{print $3}' | xargs docker rmi -f
-
+docker ps -a -q -f status=exited | grep cloudera | awk '{print $1}' | xargs docker rm
+docker images | grep cloudera | awk '{print $3}' | xargs docker rmi -f
