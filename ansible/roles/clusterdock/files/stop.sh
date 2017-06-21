91c85d1916e98f7f62f5d24538421ca7297f8d74
michal maxian <michal@maxian.sk>
2017-06-21 19:56:11 +0200

    BUILD-281 stop script changed

--- ansible/roles/clusterdock/files/stop.sh
+++ ansible/roles/clusterdock/files/stop.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker stop $(docker ps | grep cloudera | awk '{print $1}')
+psstring="cloudera"
+RET=$(docker ps | grep ${psstring} | awk '{print $1}')
+if [ ${RET} ]
+then
+  docker stop $(docker ps | grep cloudera | awk '{print $1}')
+else
+  echo "No containers marked as ${psstring}."
+fi
