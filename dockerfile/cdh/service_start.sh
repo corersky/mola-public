5a91621d88645c4589ad9a6784f83179eb2f67ba
michal maxian <michal@maxian.sk>
2017-06-21 21:02:25 +0200

    More comments added to Datameer docker start script for cdh/hdp

--- dockerfile/cdh/service_start.sh
+++ dockerfile/cdh/service_start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 echo "export DAS_DEPLOY_MODE=$DEPLOY_MODE" >> /Datameer/etc/das-env.sh && echo -e "system.property.db.host=$MYSQL_HOST\nsystem.property.db.port=$MYSQL_PORT" >> /Datameer/conf/default.properties
+echo
 cd /Datameer
+echo "Datameer application check."
+./bin/conductor.sh check
+echo "done."
+echo
+echo "Datameer application start."
 ./bin/conductor.sh start
+echo "done."
+echo 
+echo "Opening logs."
 touch logs/conductor.log
 tail -f logs/conductor.log
