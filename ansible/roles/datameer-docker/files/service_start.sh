5afa68e722feea858b99b9932fc95391cfc218cd
michal maxian <michal@maxian.sk>
2017-06-29 00:13:05 +0200

    datameer dockerfile is moved to datameer-docker path in ansible root

--- ansible/roles/datameer-docker/files/service_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "export DAS_DEPLOY_MODE=$DEPLOY_MODE" >> /Datameer/etc/das-env.sh && echo -e "system.property.db.host=$MYSQL_HOST\nsystem.property.db.port=$MYSQL_PORT" >> /Datameer/conf/default.properties
-cd /Datameer
-./bin/conductor.sh start
-touch logs/conductor.log
-tail -f logs/conductor.log
